0b30634ca64a07b1a3732b909f790b8f6d5009f9
rgroemmer <mail@rapsn.me>
2024-05-30 02:27:57 +0200

    Remove defaults from gopls config

--- langs/go.nix
+++ langs/go.nix
@@ -3,6 +3,7 @@
   # packages required in PATH for this lang
   extraPackages = with pkgs; [
     go
+    gopls
   ];
 
   plugins.lsp.servers.gopls = {
@@ -11,39 +12,10 @@
     extraOptions.settings = {
       gopls = {
         staticcheck = true;
-        directoryFilters = ["-.git" "-.vscode" "-.idea" "-.vscode-test" "-node_modules"];
+        directoryFilters = ["-.git" "-.vscode"];
         semanticTokens = true;
-        codelenses = {
-          gc_details = false;
-          generate = true;
-          regenerate_cgo = true;
-          run_govulncheck = true;
-          test = true;
-          tidy = true;
-          upgrade_dependency = true;
-          vendor = true;
-        };
-        hints = {
-          assignVariableTypes = false;
-          compositeLiteralFields = false;
-          compositeLiteralTypes = false;
-          constantValues = true;
-          functionTypeParameters = true;
-          parameterNames = true;
-          rangeVariableTypes = false;
-        };
         analyses = {
-          assign = true;
-          bools = true;
-          defers = true;
-          deprecated = true;
           fieldalignment = true;
-          tests = true;
-          nilness = true;
-          httpresponse = true;
-          unmarshal = true;
-          unusedparams = true;
-          unusedwrite = true;
           useany = true;
         };
       };
