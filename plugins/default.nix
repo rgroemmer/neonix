ecdb10bd794d33b65aa07aae892a3714c7cfa5d6
rgroemmer <mail@rapsn.me>
2024-05-30 18:17:43 +0200

    Add mini & which-key

--- plugins/default.nix
+++ plugins/default.nix
@@ -7,18 +7,28 @@
     ./treesitter.nix
     ./telescope.nix
     ./wilder.nix
+    ./which-key.nix
+    ./mini.nix
   ];
 
   colorschemes.catppuccin = {
     enable = true;
     settings = {
       flavour = "mocha";
+      integrations = {
+        alpha = true;
+        cmp = true;
+        dashboard = true;
+        illuminate.enabled = true;
+        mini.enabled = true;
+        telescope.enabled = true;
+      };
     };
   };
 
   plugins = {
     bufferline.enable = true;
-    which-key.enable = true;
+    illuminate.enable = true;
     better-escape = {
       enable = true;
       mapping = [ "jk" ];
---
+++ plugins/which-key.nix
@@ -0,0 +1,15 @@
+{
+  plugins.which-key = {
+    enable = true;
+    layout = {
+      height = {
+        min = 10;
+        max = 25;
+      };
+      width = {
+        min = 20;
+        max = 50;
+      };
+    };
+  };
+}
