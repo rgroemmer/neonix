b14cb1f4d70f71b3b20010808530e4f492e0ea85
Raphael Groemmer <mail@rapsn.me>
2024-07-23 15:39:30 +0200

    add yaml-companion

--- plugins/yaml-companion.nix
+++ plugins/yaml-companion.nix
@@ -1,10 +1,12 @@
 { pkgs, inputs, ... }:
 let
-  nvim-yaml-companion = with pkgs; vimUtils.buildVimPlugin {
-    pname = "yaml-companion";
-    version = "main";
-    src = inputs.yaml-companion;
-  };
+  nvim-yaml-companion =
+    with pkgs;
+    vimUtils.buildVimPlugin {
+      pname = "yaml-companion";
+      version = "main";
+      src = inputs.yaml-companion;
+    };
 in
 {
   extraPlugins = [ nvim-yaml-companion ];
@@ -12,27 +14,27 @@ in
   extraConfigLua =
     # lua
     ''
-local cfg = require("yaml-companion").setup({
-  schemas = {
-    {
-      name = "HelmRelease",
-      uri = "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/helmrelease-helm-v1.json",
-    },
-    {
-      name = "HelmRepository",
-      uri = "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/helmrepository-source-v1.json",
-    },
-    {
-      name = "GitRepository",
-      uri = "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/gitrepository-source-v1.json",
-    },
-    {
-      name = "FluxKustomization",
-      uri = "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/kustomization-kustomize-v1.json",
-    },
-  },
-})
+      local cfg = require("yaml-companion").setup({
+        schemas = {
+          {
+            name = "HelmRelease",
+            uri = "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/helmrelease-helm-v1.json",
+          },
+          {
+            name = "HelmRepository",
+            uri = "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/helmrepository-source-v1.json",
+          },
+          {
+            name = "GitRepository",
+            uri = "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/gitrepository-source-v1.json",
+          },
+          {
+            name = "FluxKustomization",
+            uri = "https://raw.githubusercontent.com/fluxcd-community/flux2-schemas/main/kustomization-kustomize-v1.json",
+          },
+        },
+      })
 
-require("lspconfig")["yamlls"].setup(cfg)
+      require("lspconfig")["yamlls"].setup(cfg)
     '';
 }
