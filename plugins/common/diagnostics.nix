4c99f418e69f87bdaa98dbc4adf7a42c390c3385
Raphael Groemmer <mail@rapsn.me>
2024-12-07 20:51:48 +0100

    Remove diagnostics hover overlay

--- plugins/common/diagnostics.nix
+++ plugins/common/diagnostics.nix
@@ -1,9 +1,4 @@
 {
-  #diagnostics = {
-  #  virtual_text = true;
-  #  update_in_insert = true;
-  #};
-
   extraConfigLua = ''
     -- diagnostic signs
     local signs = { Error = "💥", Warn = "🚧", Hint = "💡", Info = " " }
@@ -11,21 +6,6 @@
         local hl = "DiagnosticSign" .. type
       vim.fn.sign_define(hl, { text = icon, texthl= hl, numhl = hl })
     end
-
-    -- Show diagnostics on hover with debounce
-    vim.api.nvim_create_autocmd("CursorHold", {
-        pattern = "*",
-        callback = function()
-            -- Debounce diagnostics to avoid performance issues
-            vim.defer_fn(function()
-                vim.diagnostic.open_float(nil, {
-                    scope = "cursor",
-                    focusable = false,
-                    close_events = { "CursorMoved", "CursorMovedI", "BufHidden", "InsertCharPre", "WinLeave" },
-                })
-            end, 150) -- Delay by 150ms
-        end,
-    })
 
     -- toggle virtual_text & relativenumber
     vim.keymap.set("n", "<leader>sd", function()
