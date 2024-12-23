e3f3fb43aa7951d25cf2614a0bfa37bfeae2a2e3
Raphael Groemmer <mail@rapsn.me>
2024-12-23 22:42:53 +0100

    Add conform for yam

--- plugins/lang/yaml.nix
+++ plugins/lang/yaml.nix
@@ -1,10 +1,30 @@
-{ pkgs, config, ... }:
 {
-  plugins.lsp.servers.yamlls = {
-    enable = true;
+  pkgs,
+  config,
+  lib,
+  ...
+}: {
+  plugins = {
+    conform-nvim = {
+      settings = {
+        formatters_by_ft = {
+          yaml = ["yamlfmt"];
+        };
+        formatters = {
+          yamlfmt = {
+            command = lib.getExe pkgs.yamlfmt;
+          };
+        };
+      };
+    };
+    lsp.servers.yamlls = {
+      enable = true;
+    };
   };
 
+  extraPackages = with pkgs; [helm-ls];
+
   plugins.treesitter = {
-    grammarPackages = with config.plugins.treesitter.package.builtGrammars; [ yaml ];
+    grammarPackages = with config.plugins.treesitter.package.builtGrammars; [yaml];
   };
 }
