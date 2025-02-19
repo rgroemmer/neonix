bf412f80ae89396ac1a9ff8213e7f66f0229566b
Raphael Groemmer <mail@rapsn.me>
2025-02-19 18:51:36 +0100

    Add treesitter-text-objects

--- plugins/common/lsp/treesitter.nix
+++ plugins/common/lsp/treesitter.nix
@@ -35,5 +35,60 @@
         toml
       ];
     };
+    treesitter-textobjects = {
+      enable = true;
+      select = {
+        enable = true;
+        keymaps = {
+          "aa" = "@parameter.outer";
+          "ia" = "@parameter.inner";
+          "af" = "@function.outer";
+          "if" = "@function.inner";
+          "ac" = "@class.outer";
+          "ic" = "@class.inner";
+          "ai" = "@conditional.outer";
+          "ii" = "@conditional.inner";
+          "al" = "@loop.outer";
+          "il" = "@loop.inner";
+          "ak" = "@block.outer";
+          "ik" = "@block.inner";
+          "is" = "@statement.inner";
+          "as" = "@statement.outer";
+          "ad" = "@comment.outer";
+          "am" = "@call.outer";
+          "im" = "@call.inner";
+        };
+      };
+      move = {
+        enable = true;
+        setJumps = true;
+        gotoNextStart = {
+          "]m" = "@function.outer";
+          "]]" = "@class.outer";
+        };
+        gotoNextEnd = {
+          "]M" = "@function.outer";
+          "][" = "@class.outer";
+        };
+        gotoPreviousStart = {
+          "[m" = "@function.outer";
+          "[[" = "@class.outer";
+        };
+        gotoPreviousEnd = {
+          "[M" = "@function.outer";
+          "[]" = "@class.outer";
+        };
+      };
+
+      swap = {
+        enable = true;
+        swapNext = {
+          ")a" = "@parameter.inner";
+        };
+        swapPrevious = {
+          ")A" = "@parameter.inner";
+        };
+      };
+    };
   };
 }
